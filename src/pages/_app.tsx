aeccde62ebd02bba8ad6df882f008cd23b221616
faluna <37044099+faluna@users.noreply.github.com>
2021-01-24 22:49:18 +0900

    chore:up: Update library versions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from 'theme-ui';
+import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../utils/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Head>
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </ThemeProvider>
   );
 }
